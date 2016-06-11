a60f0419ebec1cd1eccc72cb4670916ad6baba74
yasuaki.ohama <yasuaki.ohama@gmail.com>
2016-06-12 07:01:08 +0900

    購入ボタンを押すとYes Noウィンドウが開くようにする

--- Assets/Scripts/CreateShopItem.cs
+++ Assets/Scripts/CreateShopItem.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 using System.Collections;
 
 public class CreateShopItem : MonoBehaviour
 {
     public int num;
+    public GameObject yesNoCanvas;
+    public Transform shopContent;
 
     void Start ()
     {
         for (int i = 0, n = num; i < n; i++) {
             var item = Instantiate (Resources.Load ("Prefabs/ItemButton")) as GameObject;
             item.name = "ItemButton";
-            item.transform.SetParent (transform, false);
+            item.transform.SetParent (shopContent, false);
+
+            item.transform.FindChild ("Button").GetComponent<Button> ().onClick.AddListener (() => {
+                yesNoCanvas.SetActive (true);
+            });
         }
     }
 }
