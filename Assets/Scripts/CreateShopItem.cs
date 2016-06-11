2f0a611b390f8dff8780f098102edcfd3c406dc8
yasuaki.ohama <yasuaki.ohama@gmail.com>
2016-06-12 06:47:33 +0900

    アイテムボタンをスクリプトから作成

---
+++ Assets/Scripts/CreateShopItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateShopItem : MonoBehaviour
+{
+    public int num;
+
+    void Start ()
+    {
+        for (int i = 0, n = num; i < n; i++) {
+            var item = Instantiate (Resources.Load ("Prefabs/ItemButton")) as GameObject;
+            item.name = "ItemButton";
+            item.transform.SetParent (transform, false);
+        }
+    }
+}
